d9a13b356ad359dcf3a4ad5397fd6c7b37c49fae
pankaja455 <pankaja455@gmail.com>
2016-06-05 17:47:26 +0530

    first composer package

---
+++ src/Services/CCDAService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace laravelccda\Services;
+
+class CCDAService {
+
+	public function __construct() {
+
+	}
+
+	public function saveCCDAAsJson($xmlfilePath){
+		$jsonfilename = str_random(9) . ".json";
+		$jsonFile = public_path() . '/' . $jsonfilename;
+		$xmlToJson = exec("node " . app_path() . "/tojson.js " . $xmlfilePath . " " . $jsonFile);
+		$jsonString = file_get_contents($jsonFile, true);
+		$validator = \Validator::make(array('jsson' => $jsonString), array('jsson' => 'Required|json'));
+		if ($validator->fails())
+			return 'Invalid File';
+		unlink($jsonFile);
+		return $jsonString;
+	}
+
+}
+
+
+?>
